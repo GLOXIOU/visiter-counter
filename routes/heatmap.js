454c01338d68f3163c54eb89b799e9fcfeaa6979
llex_tv <leo.gibouin@orange.fr>
2025-11-23 15:07:30 +0100

    changes and adding base of heatmap and backend

--- routes/heatmap.js
+++ routes/heatmap.js
@@ -1,20 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/:id', (req,res) => {
-
-});
-
-router.post('/:id', (req,res) => {
-
-});
-
-router.delete('/:id', (req,res) => {
-
-});
-
-router.patch('/:id', (req,res) => {
 
 });
 
